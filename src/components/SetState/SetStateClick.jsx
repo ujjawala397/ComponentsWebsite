42683d9b958e1bd384087e651f62f4deb11f11ae
ujjawala397 <ujjawala397@gmail.com>
2022-09-30 17:46:22 -0400

    State change by click functional component

--- src/components/SetState/SetStateClick.jsx
+++ src/components/SetState/SetStateClick.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { Button, Input } from '@mui/material'
-
+import { Link } from 'react-router-dom';
 
 
 function SetStateClick() {
@@ -13,6 +13,9 @@ function SetStateClick() {
     
     return (
         <div>
+            <div>
+                <Link to="/">Go back</Link>
+            </div>
             <form onSubmit={handleSubmit}>
                 <Input onChange={(e)=>{ setData ({ listItem:e.target.value }) }}/>
                 <Button type="submit">Submit</Button>
