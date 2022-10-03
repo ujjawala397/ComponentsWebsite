a980d9dfadd44636b35614f4d2e74539ccd3ebb8
ujjawala397 <ujjawala397@gmail.com>
2022-10-03 06:42:49 -0400

    Basic ToDo list app

--- src/components/TodoApp/Todo.jsx
+++ src/components/TodoApp/Todo.jsx
@@ -1,29 +1,58 @@
 import { Button } from '@mui/material'
 import React,{useState} from 'react'
-import TodoList from './TodoList'
 import { Link } from 'react-router-dom'
 const Todo = () => {
     
-    const [data,setData]=useState({listItem:'',work:''})
+    const [data,setData] = useState('')
+    const [items,setItems] = useState([])
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(data.listItem)
+        console.log(data)
       }
 
+    const addItem = () => {
+      if(data){
+        setItems([...items,data])
+        setData('')
+      }}
+    const removeItem = (id) => {
+      const updatedItems=items.filter((item,index)=>{
+        return index!==id;
+      })
+      setItems(updatedItems);
+      console.log(updatedItems);
+    }
+    const removeAllItems=()=>{
+      setItems([]);
+    }
   return (
     <div>
-      
+        
         <div>
           <Link to="/">Go back</Link>
         </div>
+
         <form onSubmit={handleSubmit}>
-            <input placeholder='enter task' value={data.listItem} onChange={(e)=>(setData({...data,listItem:e.target.value}))} />
-            <br/>
-            <input placeholder='enter work' value={data.work} onChange={(e)=>(setData({...data,work:e.target.value}))} />
-            <Button type="submit">Add</Button>
+            <input 
+              placeholder='enter task' 
+              type="text" 
+              value={data} 
+              onChange={(e)=>setData(e.target.value)} />
+
+            
+              <Button onClick={addItem}>Add</Button>
+            <div>
+              {items.map((item,index)=>(
+                <div key={index}>
+                   {item} <Button onClick={()=>removeItem(index)}> Delete</Button>
+
+                </div>
+              ))}
+            </div>
+            
+            <Button onClick={removeAllItems}>Remove All</Button>
         </form>
 
-        <TodoList name={data.listItem}/>
     </div>
   )
 }
