8d5e135fb913c2b9c399323f1ef4032a512aa37d
ujjawala397 <ujjawala397@gmail.com>
2022-10-09 14:10:52 -0400

    sign up page structure built

--- src/components/Login/SignupForm.jsx
+++ src/components/Login/SignupForm.jsx
@@ -4,20 +4,39 @@ import { Input,Button } from '@mui/material';
 import { useState } from 'react';
 const SignupForm = () => {
 
-    const [data,setData]=useState([]);
-
-    function handleSubmit(){
-
+    const [data,setData]=useState({name:'',email:'',pass:''});
+    function handleSubmit(e){
+        
+        e.preventDefault();
+        console.log(data)
+        setData({name:'',email:'',pass:''})
+        
     }
+
   return (
     <div>
         <Link to="/">Home</Link>
+
         <form onSubmit={handleSubmit}>
-            <Input placeholder='Enter Name' onChange={(e)=>(setData({...data,name:e.target.value}))}/>
+            <Input 
+                placeholder='Enter Name'  
+                type="text"
+                value={data.name} 
+                onChange={(e)=>(setData({...data,name:e.target.value}))}
+            />
             <br></br>   
-            <Input placeholder='Enter Email'onChange={(e)=>(setData({...data,email:e.target.value}))}/>
+            <Input 
+                placeholder='Enter Email' 
+                type="text"
+                value={data.email} 
+                onChange={(e)=>(setData({...data,email:e.target.value}))}
+            />
             <br></br>
-            <Input placeholder='Enter password' type="password" onChange={(e)=>(setData({...data,pass:e.target.value}))}/>
+            <Input 
+                placeholder='Enter password' 
+                value={data.pass} 
+                type="password"
+                onChange={(e)=>(setData({...data,pass:e.target.value}))}/>
             <br></br>
             <Button type ="submit">SignUp</Button>
         </form>
